d2b1b5346700c4baf9c3d01bdad070231046c0c4
Chinmay Kulkarni <kulkarnichinmay65@gmail.com>
2024-08-14 16:13:11 +0530

    Update run_webapp.cmd

--- run_webapp.cmd
+++ run_webapp.cmd
@@ -1,6 +1,13 @@
 @echo off
 if not exist "chatbot" (
+    echo "Creating venv..."
     python -m venv chatbot
+    echo "Activating venv..."
+    call chatbot\Scripts\activate
+    echo "Installing deps..."
+    pip install -r requirements.txt
 )
+echo "Activating venv..."
 call chatbot\Scripts\activate
+echo "Starting App..."
 python app.py
